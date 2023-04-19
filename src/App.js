170a6bde72e289b4f265cbdac001fd35f45e798e
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2023-04-19 23:56:21 +0600

    seting up tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className="text-4xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-2xl font-bold leading-[200px]">Hello world!</h1>;
 }
